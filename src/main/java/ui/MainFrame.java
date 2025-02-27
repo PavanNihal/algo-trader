6e4a187800ebf2aae2e0c4fb0bb01001e9d9b1ce
PavanNihal <cpnihal2011@gmail.com>
2025-02-27 11:44:06 +0530

    Remove unreachable catch block for AccessTokenExpiredException

--- src/main/java/ui/MainFrame.java
+++ src/main/java/ui/MainFrame.java
@@ -2,7 +2,6 @@ package ui;
 
 import api.LiveFeedManager;
 import api.LiveFeederFactory;
-import authentication.AccessTokenExpiredException;
 import database.DatabaseManager;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
@@ -19,11 +18,7 @@ public class MainFrame extends StackPane {
         
         // Initialize LiveFeedManager with token
         LiveFeedManager liveFeedManager = LiveFeederFactory.getInstance();
-        try {
-            liveFeedManager.setAccessToken(authToken);
-        } catch (AccessTokenExpiredException e) {
-            e.printStackTrace();
-        }
+        liveFeedManager.setAccessToken(authToken);
         
         // Initialize main application components with the token
         homePanel = new HomePanel(dbManager, liveFeedManager);
